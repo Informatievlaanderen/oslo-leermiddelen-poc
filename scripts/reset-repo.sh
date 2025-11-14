83be1952b22a2c845783aa4b58e9e71fd5298586
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2025-11-14 13:01:55 +0100

    Initial commit

---
+++ scripts/reset-repo.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SCRIPT_DIR/reset-readme.sh
+rm -rf example-data \
+  mappings \
+  in-shacl \
+  data \
+  docs \
+  output \
+  template.xlsx \
+  env.txt
+
+mkdir docs
+cp $SCRIPT_DIR/docs-placeholder.html $SCRIPT_DIR/../docs/index.html
